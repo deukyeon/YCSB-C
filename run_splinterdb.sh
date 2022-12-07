6132a36b567563c6780d358a5d8d10536f4577fa
deukyeon <deukyeon.hwang@gmail.com>
2022-12-07 07:56:30 +0000

    Update run scripts

--- run_splinterdb.sh
+++ run_splinterdb.sh
@@ -4,24 +4,26 @@ if [[ "x$1" == "xsplinterdb" || "x$1" == "xtransactional_splinterdb" ]]
 then
   DB=$1
 else
-  echo "Usage: $0 [splinterdb|transactional_splinterdb]"
+  echo "Usage: $0 [splinterdb|transactional_splinterdb] [(optional)label]"
   exit 1
 fi
 
-THREADS=(1 2 4 8)
+LABEL=${2:-$DB}
+
+THREADS=(1 2 4 8 12 16 20 24 28 32)
 
 echo "Run for the uniform distribution"
 
-OUT=uniform.out
+OUT=$LABEL-uniform.out
 
 rm -f $OUT
 
 for t in ${THREADS[@]}
 do
-  bash run_individual.sh $DB $t uniform >> $OUT 2>&1
+  bash run_individual.sh $DB $t uniform 100 >> $OUT 2>&1
 done
 
-OUT=zipf.out
+OUT=$LABEL-zipf.out
 
 rm -f $OUT
 
@@ -29,5 +31,5 @@ echo "Run for the zipfian distribution"
 
 for t in ${THREADS[@]}
 do
-  bash run_individual.sh $DB $t zipfian >> $OUT 2>&1
+  bash run_individual.sh $DB $t zipfian 100 >> $OUT 2>&1
 done
