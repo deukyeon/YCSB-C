c4d358a2f3c274c64e1babcaa93579d4ef8e6e45
deukyeon <deukyeon.hwang@gmail.com>
2023-01-05 02:43:04 +0000

    Update sciprts

--- run_splinterdb.sh
+++ run_splinterdb.sh
@@ -4,34 +4,47 @@ if [[ "x$1" == "xsplinterdb" || "x$1" == "xtransactional_splinterdb" ]]
 then
   DB=$1
 else
-  echo "Usage: $0 [splinterdb|transactional_splinterdb] [(optional)fieldlength] [(optional)label]"
+  echo "Usage: $0 [splinterdb|transactional_splinterdb] [(optional)label]"
   exit 1
 fi
+LABEL=${2:-$DB}
 
-LABEL=${3:-$DB}
-
-FIELDLENGTH=${2:-1024}
-
+FIELDLENGTH=100
 THREADS=(1 2 4 8 12 16 20 24 28 32)
+OPSPERTXN=16
+THETA=0.9
 
-echo "Run for the uniform distribution"
+REPEAT=5
 
-OUT=~/$LABEL-uniform.out
+echo "Experiments for $LABEL"
 
-rm -f $OUT
-
-for t in ${THREADS[@]}
+for i in $(seq $REPEAT)
 do
-  bash run_individual.sh $DB $t uniform $FIELDLENGTH >> $OUT 2>&1
-done
+    echo "Run for the uniform distribution"
 
-OUT=~/$LABEL-zipf.out
+    OUT=~/$LABEL-uniform.out.$i
 
-rm -f $OUT
+    rm -f $OUT
 
-echo "Run for the zipfian distribution"
+    for t in ${THREADS[@]}
+    do
+	bash run_individual.sh $DB $t uniform $FIELDLENGTH $OPSPERTXN 0 >> $OUT 2>&1
+    done
 
-for t in ${THREADS[@]}
-do
-  bash run_individual.sh $DB $t zipfian $FIELDLENGTH >> $OUT 2>&1
+    echo "Output: $OUT"
+    echo "To parse the result, run 'python plot_throughput.py $OUT'"
+
+    OUT=~/$LABEL-zipf.out.$i
+
+    rm -f $OUT
+
+    echo "Run for the zipfian distribution (theta=$THETA)"
+
+    for t in ${THREADS[@]}
+    do
+	bash run_individual.sh $DB $t zipfian $FIELDLENGTH $OPSPERTXN $THETA >> $OUT 2>&1
+    done
+
+    echo "Output: $OUT"
+    echo "To parse the result, run 'python plot_throughput.py $OUT'"
 done
