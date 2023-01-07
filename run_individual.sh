c693b91eef3ca81387bfa53d5600cce0a43cc751
deukyeon <deukyeon.hwang@gmail.com>
2023-01-07 11:31:19 -0800

    Update the run_individual.sh

--- run_individual.sh
+++ run_individual.sh
@@ -10,17 +10,29 @@ test "$FIELDLENGTH" == "100" && RECORDCOUNT=100000000
 OPSPERTRANSACTION=${5:-16}
 #TRANSACTIONCOUNT=10000
 #OPERATIONCOUNT=$(($OPSPERTRANSACTION * $TRANSACTIONCOUNT))
-TXNPERTHREAD=1000
+TXNPERTHREAD=10000
 OPERATIONCOUNT=$(($OPSPERTRANSACTION * $THREADS * $TXNPERTHREAD))
 
 THETA=${6:-0.9}
 
-gdb --args ./ycsbc -db $DB -threads $THREADS \
--L workloads/load.spec \
+LOADSPEC=workloads/load.spec
+WORKLOADSPEC=workloads/workloada.spec
+
+RUN="./ycsbc -db $DB -threads $THREADS \
+-L $LOADSPEC \
 -w fieldlength $FIELDLENGTH \
 -w recordcount $RECORDCOUNT \
--W workloads/workloada.spec \
+-W $WORKLOADSPEC \
 -w requestdistribution $DIST \
 -w operationcount $OPERATIONCOUNT \
 -w opspertransaction $OPSPERTRANSACTION \
--w theta $THETA
+-w theta $THETA"
+
+echo "=== Run command ==="
+echo $RUN
+echo "--- Load file ($LOADSPEC) ---"
+cat $LOADSPEC
+echo "--- Workload file ($WORKLOADSPEC) ---"
+cat $WORKLOADSPEC
+echo "=== Running the benchamrk ==="
+$RUN
