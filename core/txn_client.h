268aaad70a9646440778c524652dc48c8453e848
deukyeon <deukyeon.hwang@gmail.com>
2023-08-14 07:25:40 +0000

    Update txn_client.h

--- core/txn_client.h
+++ core/txn_client.h
@@ -13,55 +13,55 @@ protected:
    GenerateClientTransactionalOperations()
    {
       ClientOperation client_op;
-      size_t          num_ops          = workload_.ops_per_transaction();
-      size_t          num_ops_to_store = 0;
+      size_t          num_ops = workload_.ops_per_transaction();
 
       if (workload_.read_proportion() > 0) {
-         int read_ops = num_ops * workload_.read_proportion() + 0.5;
+         size_t read_ops = num_ops * workload_.read_proportion() + 0.5;
          read_ops += (read_ops == 0);
-         num_ops_to_store += read_ops;
-         while (operations_in_transaction.size() < num_ops_to_store) {
+         size_t start_num_ops = operations_in_transaction.size();
+         while (operations_in_transaction.size() - start_num_ops < read_ops) {
             GenerateClientOperationRead(client_op);
             operations_in_transaction.emplace(client_op);
          }
       }
 
       if (workload_.insert_proportion() > 0) {
-         int insert_ops = num_ops * workload_.insert_proportion() + 0.5;
+         size_t insert_ops = num_ops * workload_.insert_proportion() + 0.5;
          insert_ops += (insert_ops == 0);
-         num_ops_to_store += insert_ops;
-         while (operations_in_transaction.size() < num_ops_to_store) {
+         size_t start_num_ops = operations_in_transaction.size();
+         while (operations_in_transaction.size() - start_num_ops < insert_ops) {
             GenerateClientOperationInsert(client_op);
             operations_in_transaction.emplace(client_op);
          }
       }
 
       if (workload_.update_proportion() > 0) {
-         int update_ops = num_ops * workload_.update_proportion() + 0.5;
+         size_t update_ops = num_ops * workload_.update_proportion() + 0.5;
          update_ops += (update_ops == 0);
-         num_ops_to_store += update_ops;
-         while (operations_in_transaction.size() < num_ops_to_store) {
+         size_t start_num_ops = operations_in_transaction.size();
+         while (operations_in_transaction.size() - start_num_ops < update_ops) {
             GenerateClientOperationUpdate(client_op);
             operations_in_transaction.emplace(client_op);
          }
       }
 
       if (workload_.scan_proportion() > 0) {
-         int scan_ops = num_ops * workload_.scan_proportion() + 0.5;
+         size_t scan_ops = num_ops * workload_.scan_proportion() + 0.5;
          scan_ops += (scan_ops == 0);
-         num_ops_to_store += scan_ops;
-         while (operations_in_transaction.size() < num_ops_to_store) {
+         size_t start_num_ops = operations_in_transaction.size();
+         while (operations_in_transaction.size() - start_num_ops < scan_ops) {
             GenerateClientOperationScan(client_op);
             operations_in_transaction.emplace(client_op);
          }
       }
 
       if (workload_.readmodifywrite_proportion() > 0) {
-         int readmodifywrite_ops =
+         size_t readmodifywrite_ops =
             num_ops * workload_.readmodifywrite_proportion() + 0.5;
          readmodifywrite_ops += (readmodifywrite_ops == 0);
-         num_ops_to_store += readmodifywrite_ops;
-         while (operations_in_transaction.size() < num_ops_to_store) {
+         size_t start_num_ops = operations_in_transaction.size();
+         while (operations_in_transaction.size() - start_num_ops
+                < readmodifywrite_ops) {
             GenerateClientOperationReadModifyWrite(client_op);
             operations_in_transaction.emplace(client_op);
          }
