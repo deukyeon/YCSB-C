2a0fda8c633a8ed373495160ab7cf76e16090106
deukyeon <deukyeon.hwang@gmail.com>
2023-08-23 07:19:34 +0000

    Update run_sketch_size_exp.py

--- run_sketch_size_exp.py
+++ run_sketch_size_exp.py
@@ -1,6 +1,16 @@
 #!/usr/bin/env python3
 
 import os
+import signal
+import sys
+
+def signal_handler(sig, frame):
+    print("Received signal:", sig)
+    print("Exiting...")
+    sys.exit(0)
+
+# Register the signal handler for SIGINT
+signal.signal(signal.SIGINT, signal_handler)
 
 ycsb_path = os.getcwd()
 splinterdb_path = "../splinterdb"
@@ -15,11 +25,10 @@ os.system("git checkout src/experimental_mode.h")
 os.system("sed -i 's/#define EXPERIMENTAL_MODE_TICTOC_SKETCH [ ]*0/#define EXPERIMENTAL_MODE_TICTOC_SKETCH 1/g' src/experimental_mode.h")
 
 for rows in [1, 2, 4]:
+    os.system(f"sed -i 's/txn_splinterdb_cfg->sktch_config.rows = [0-9]\+;/txn_splinterdb_cfg->sktch_config.rows = {rows};/' src/transaction_impl/transaction_tictoc_sketch.h")
     cols = 262144
     while cols >= 1:
-        os.system(f"sed -i 's/txn_splinterdb_cfg->sktch_config.rows = [0-9]\+;/txn_splinterdb_cfg->sktch_config.rows = {rows};/' src/transaction_impl/transaction_tictoc_sketch.h")
         os.system(f"sed -i 's/txn_splinterdb_cfg->sktch_config.cols = [0-9]\+;/txn_splinterdb_cfg->sktch_config.cols = {cols};/' src/transaction_impl/transaction_tictoc_sketch.h")
-        cols = cols // 2
 
         os.system("sudo -E make clean")
         os.system("sudo -E make install")
@@ -29,3 +38,5 @@ for rows in [1, 2, 4]:
 
         output_path = os.path.join(results_path, f"rows_{rows}_cols_{cols}")
         os.system(f"LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so ./ycsbc -db transactional_splinterdb -threads 60 -L workloads/read_intensive.spec -p splinterdb.filename workloads/read_intensive.spec -p splinterdb.cache_size_mb 4096 -p splinterdb.filename /dev/md0 > {output_path} 2>&1")
+
+        cols = cols // 2
