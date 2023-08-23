2f7f1922189b5d740635ef9af3c3419547e148f5
deukyeon <deukyeon.hwang@gmail.com>
2023-08-23 07:14:33 +0000

    Add a script for exps with varying sketch size

---
+++ run_sketch_size_exp.py
@@ -0,0 +1,31 @@
+#!/usr/bin/env python3
+
+import os
+
+ycsb_path = os.getcwd()
+splinterdb_path = "../splinterdb"
+
+results_path = os.path.join(ycsb_path, "sketch_size_exp")
+if not os.path.exists(results_path):
+    os.mkdir(results_path)
+
+# assume the branch of splinterdb is deukyeon/fantastiCC-refactor
+os.chdir(splinterdb_path)
+os.system("git checkout src/experimental_mode.h")
+os.system("sed -i 's/#define EXPERIMENTAL_MODE_TICTOC_SKETCH [ ]*0/#define EXPERIMENTAL_MODE_TICTOC_SKETCH 1/g' src/experimental_mode.h")
+
+for rows in [1, 2, 4]:
+    cols = 262144
+    while cols >= 1:
+        os.system(f"sed -i 's/txn_splinterdb_cfg->sktch_config.rows = [0-9]\+;/txn_splinterdb_cfg->sktch_config.rows = {rows};/' src/transaction_impl/transaction_tictoc_sketch.h")
+        os.system(f"sed -i 's/txn_splinterdb_cfg->sktch_config.cols = [0-9]\+;/txn_splinterdb_cfg->sktch_config.cols = {cols};/' src/transaction_impl/transaction_tictoc_sketch.h")
+        cols = cols // 2
+
+        os.system("sudo -E make clean")
+        os.system("sudo -E make install")
+        os.chdir(ycsb_path)
+        os.system("make clean")
+        os.system("make")
+
+        output_path = os.path.join(results_path, f"rows_{rows}_cols_{cols}")
+        os.system(f"LD_PRELOAD=/usr/lib/x86_64-linux-gnu/libjemalloc.so ./ycsbc -db transactional_splinterdb -threads 60 -L workloads/read_intensive.spec -p splinterdb.filename workloads/read_intensive.spec -p splinterdb.cache_size_mb 4096 -p splinterdb.filename /dev/md0 > {output_path} 2>&1")
