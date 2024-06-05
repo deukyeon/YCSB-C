8e227168d98997153d6594e971257c5ff546130d
Deukyeon Hwang <deukyeon.hwang@gmail.com>
2024-06-05 01:15:46 -0700

    Update ycsb.sh

--- ycsb.sh
+++ ycsb.sh
@@ -3,15 +3,15 @@
 set -e
 
 SYSTEMS=(mvcc-memory mvcc-sketch mvcc-counter)
-WORKLOADS=(write_intensive)
+WORKLOADS=(write_intensive read_intensive)
 NRUNS=2
 
-for thr in 1 2 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
-do
+for work in ${WORKLOADS[@]}
+do 
     for sys in ${SYSTEMS[@]}
     do
-        for work in ${WORKLOADS[@]}
-        do 
+	for thr in 1 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
+	do
             for run in $(seq 1 ${NRUNS})
             do
                 ./ycsb.py -s $sys -w $work -t $thr -c 6144 -r 60 > $HOME/${sys}_${work}_${thr}_${run}.log
