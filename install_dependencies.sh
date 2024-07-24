7b8e3efca9c014e7feffce2ae5f60f7639497e71
deukyeon <deukyeon.hwang@gmail.com>
2024-07-24 14:57:43 +0000

    Updated install_dependencies.sh

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -2,4 +2,7 @@ wget -O - https://apt.llvm.org/llvm-snapshot.gpg.key | sudo apt-key add -
 sudo add-apt-repository 'deb http://apt.llvm.org/focal/ llvm-toolchain-focal-13 main'
 sudo apt update
 sudo apt install -y build-essential libtbb-dev librocksdb-dev libhiredis-dev libaio-dev libconfig-dev libxxhash-dev libssl-dev libnuma-dev libjemalloc-dev clang-13 clang-format-13 python3 python3-pip
+if [ ! -f /usr/bin/clang ]; then
+    sudo ln -s /usr/bin/clang-13 /usr/bin/clang
+fi
 python3 -mpip install pandas
