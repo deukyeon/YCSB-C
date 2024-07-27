7a3113f698867c3310ef69010d30107d2826e317
deukyeon <deukyeon.hwang@gmail.com>
2024-07-27 16:38:22 +0000

    Add tpcc scripts

---
+++ tpcc.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/bash -x
+
+SYSTEMS=(2pl-no-wait baseline-serial baseline-parallel sto-disk sto-memory sto-counter sto-sketch tictoc-disk tictoc-memory tictoc-counter tictoc-sketch)
+#  mvcc-memory mvcc-sketch mvcc-counter mvcc-disk will be added later.
+WORKLOADS=(tpcc-wh4 tpcc-wh8 tpcc-wh16 tpcc-wh32 tpcc-wh4-upserts tpcc-wh8-upserts tpcc-wh16-upserts tpcc-wh32-upserts)
+NRUNS=2
+
+LOG_DIR=$HOME/tpcc_logs
+OUTPUT_DIR=$HOME/tpcc_results
+
+mkdir -p $LOG_DIR
+
+for work in ${WORKLOADS[@]}
+do 
+    for sys in ${SYSTEMS[@]}
+    do
+	for thr in 1 4 8 12 16 20 24 28 32 36 40 44 48 52 56 60
+	do
+            for run in $(seq 1 ${NRUNS})
+            do
+                ./tpcc.py -s $sys -w $work -t $thr -c 6144 -r 60 -d /dev/md0 > $LOG_DIR/${sys}_${work}_${thr}_${run}.log
+            done
+        done
+    done
+done
+
+mkdir -p $OUTPUT_DIR
+python3 parse.py $LOG_DIR $OUTPUT_DIR
