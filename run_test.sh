6b185ea57d027b91ba691897e22c50e21fe855ca
deukyeon <deukyeon@node0.deukyeon-233747.tasrdma-pg0.clemson.cloudlab.us>
2024-11-30 00:18:05 -0500

    Update run_test.sh

--- run_test.sh
+++ run_test.sh
@@ -1,4 +1,6 @@
-mkdir -p newbaseline
+LOG_DIR=$HOME/newbaseline
+
+mkdir -p $LOG_DIR
 
 for prot in tictoc sto mvcc
 do
@@ -6,7 +8,7 @@ do
     do
         for work in write_intensive read_intensive
         do
-            LOG_FILE="newbaseline/$prot-$sys-$work"
+            LOG_FILE="$LOG_DIR/$prot-$sys-$work"
             # Skip if log file already exists and contains the desired line
             if [ -f "$LOG_FILE" ] && grep -q "# Transaction throughput (KTPS)" "$LOG_FILE"; then
                 continue
@@ -17,7 +19,7 @@ do
             # Retry until the output file contains the desired line
             while true
             do
-                python3 ycsb.py -s $prot-$sys -w $work -d /dev/sdb2 -t 60 -r 240 > $LOG_FILE
+                python3 ycsb.py -s $prot-$sys -w $work -d /dev/nvme0n1 -c 6144 -t 60 -r 240 > $LOG_FILE
 
                 # Check if the log file contains the required line
                 if grep -q "# Transaction throughput (KTPS)" "$LOG_FILE"; then
