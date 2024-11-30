98c6230bcd52b49c7ebd2a737c8f5f505fcbc627
deukyeon <deukyeon.hwang@gmail.com>
2024-11-30 05:07:49 +0000

    Add run_test.sh, a temp script to test hash table

---
+++ run_test.sh
@@ -0,0 +1,29 @@
+mkdir -p newbaseline
+
+for prot in tictoc sto mvcc
+do
+    for sys in memory sketch counter hashtable
+    do
+        for work in write_intensive read_intensive
+        do
+            LOG_FILE="newbaseline/$prot-$sys-$work"
+            # Skip if log file already exists and contains the desired line
+            if [ -f "$LOG_FILE" ] && grep -q "# Transaction throughput (KTPS)" "$LOG_FILE"; then
+                continue
+            fi
+
+            echo "python3 ycsb.py -s $prot-$sys -w $work -d /dev/sdb2 -t 60 -r 240"
+
+            # Retry until the output file contains the desired line
+            while true
+            do
+                python3 ycsb.py -s $prot-$sys -w $work -d /dev/sdb2 -t 60 -r 240 > $LOG_FILE
+
+                # Check if the log file contains the required line
+                if grep -q "# Transaction throughput (KTPS)" "$LOG_FILE"; then
+                    break
+                fi
+            done
+        done
+    done
+done
