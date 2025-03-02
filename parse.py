fdb8961b98e5d4a6758c16d0eaf9d99c5a141018
deukyeon <deukyeon.hwang@gmail.com>
2025-03-02 17:17:36 +0000

    Update parse.py

--- parse.py
+++ parse.py
@@ -10,7 +10,8 @@ def parse_input_file(file_path):
 
     # Define regex patterns for the required information
     load_throughput_pattern = r"# Load throughput \(KTPS\)\n.*\s+(\d+\.\d+)"
-    thread_goodput_pattern = r"# Transaction throughput \(KTPS\)\n.*\s+(\d+)\s+(\d+\.\d+)"
+    thread_count_pattern = r"# Number of client threads:\s+(\d+)"
+    thread_goodput_pattern = r"# Transaction throughput \(KTPS\)\s+(\d+\.\d+)"
     abort_count_pattern = r"# Abort count:\s+(\d+)"
     abort_rate_pattern = r"Abort rate:\s+(.+)"
     commit_latency_pattern = r"# Commit Latencies \(us\)\nMin:\s+(.+)\nMax:\s+(.+)\nAvg:\s+(.+)\nP50:\s+(.+)\nP90:\s+(.+)\nP95:\s+(.+)\nP99:\s+(.+)\nP99\.9:\s+(.+)"
@@ -20,13 +21,11 @@ def parse_input_file(file_path):
     load_throughput = re.search(load_throughput_pattern, input_text)
     load_throughput = load_throughput.group(1) if load_throughput else 0
 
-    thread_goodput = re.search(thread_goodput_pattern, input_text)
-    if thread_goodput:
-        thread_count = thread_goodput.group(1)
-        goodput = thread_goodput.group(2)
-    else:
-        thread_count = 0
-        goodput = 0
+    thread_count = re.search(thread_count_pattern, input_text)
+    thread_count = thread_count.group(1) if thread_count else 0
+
+    goodput = re.search(thread_goodput_pattern, input_text)
+    goodput = goodput.group(1) if goodput else 0
 
     abort_count = re.search(abort_count_pattern, input_text)
     abort_count = abort_count.group(1) if abort_count else 0
